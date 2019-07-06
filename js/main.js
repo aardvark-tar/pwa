aaa88b22120d0454c6422fdef7c0d22f299c6447
aardvark-tar <aardvark.toh@gmail.com>
2019-07-06 11:50:38 +0800

    2

--- js/main.js
+++ js/main.js
@@ -3,23 +3,23 @@ window.onload = () => {
 
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-             .register('./sw.js');
+      .register('./sw.js');
   }
 
 
-if ('DeviceOrientationEvent' in window) {
-  window.addEventListener('deviceorientation', deviceOrientationHandler, false);
-} else {
-  document.getElementById('logoContainer').innerText = 'Device Orientation API not supported.';
-}
+  if ('DeviceOrientationEvent' in window) {
+    window.addEventListener('deviceorientation', deviceOrientationHandler, false);
+  } else {
+    document.getElementById('logoContainer').innerText = 'Device Orientation API not supported.';
+  }
 
 }
 
-function deviceOrientationHandler (eventData) {
+function deviceOrientationHandler(eventData) {
   var tiltLR = eventData.gamma;
   var tiltFB = eventData.beta;
   var dir = eventData.alpha;
-  
+
   document.getElementById("doTiltLR").innerHTML = Math.round(tiltLR);
   document.getElementById("doTiltFB").innerHTML = Math.round(tiltFB);
   document.getElementById("doDirection").innerHTML = Math.round(dir);
@@ -29,5 +29,3 @@ function deviceOrientationHandler (eventData) {
   logo.style.MozTransform = "rotate(" + tiltLR + "deg)";
   logo.style.transform = "rotate(" + tiltLR + "deg) rotate3d(1,0,0, " + (tiltFB * -1) + "deg)";
 }
-
-
