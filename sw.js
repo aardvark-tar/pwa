ff70360509e9d9394b6fe25f08c9c4fef17f91a3
aardvark-tar <aardvark.toh@gmail.com>
2020-01-07 18:27:50 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,20 +6,56 @@ var filesToCache = [
   './js/main.js'
 ];
 
-/* Start the service worker and cache all of the app's content */
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(cacheName)
+        .then(function(cache) {
+            console.info('[sw.js] cached all files');
+            return cache.addAll(filesToCache);
+        })
+    );
 });
 
-/* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request)
+        .then(function(response) {
+            if(response){
+                return response
+            }
+            else{
+                // clone request stream
+                // as stream once consumed, can not be used again
+                var reqCopy = event.request.clone();
+                
+                return fetch(reqCopy, {credentials: 'include'}) // reqCopy stream consumed
+                .then(function(response) {
+                    // bad response
+                    // response.type !== 'basic' means third party origin request
+                    if(!response || response.status !== 200 || response.type !== 'basic') {
+                        return response; // response stream consumed
+                    }
+
+                    // clone response stream
+                    // as stream once consumed, can not be used again
+                    var resCopy = response.clone();
+
+
+                    // ================== IN BACKGROUND ===================== //
+
+                    // add response to cache and return response
+                    caches.open(cacheName)
+                    .then(function(cache) {
+                        return cache.put(reqCopy, resCopy); // reqCopy, resCopy streams consumed
+                    });
+
+                    // ====================================================== //
+
+
+                    return response; // response stream consumed
+                })
+            }
+        })
+    );
 });
