569f18d476d654833a3c508f9f99215f44926304
aardvark-tar <aardvark.toh@gmail.com>
2020-01-07 18:19:37 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,5 +4,10 @@ window.onload = () => {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
       .register('./sw.js');
+    
+    //navigator.serviceWorker.ready always resolve
+    navigator.serviceWorker.ready.then(function (registration) {
+        console.log('Service worker successfully registered on scope', registration.scope);
+    });
   }
 }
